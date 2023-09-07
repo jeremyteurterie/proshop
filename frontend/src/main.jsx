558ecb4667c1991c2e50368a1f0deb88f1e3b47b
Jeremy <jeremy.teurterie@gmail.com>
2023-09-07 11:09:56 +0700

    :sparkles: Feat: redux store & api slice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 import Router from './router.jsx';
+import store from './store.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>
 );
