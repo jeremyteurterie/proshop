09fe483baa7e6bb06f31c07eb59e7f4b9f723bd6
Jeremy <jeremy.teurterie@gmail.com>
2023-09-08 09:35:16 +0700

    :sparkles: Feat: products api slice & get products endpoint

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,13 @@
+import { PRODUCT_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const productApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({ url: PRODUCT_URL }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productApiSlice;
