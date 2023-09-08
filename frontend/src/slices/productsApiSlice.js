38a6dbef6c533bd12628414f002736b0debb3ebb
Jeremy <jeremy.teurterie@gmail.com>
2023-09-08 10:29:02 +0700

    :sparkles: Feat: get product details endpoint challenge

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,7 +7,14 @@ export const productApiSlice = apiSlice.injectEndpoints({
       query: () => ({ url: PRODUCT_URL }),
       keepUnusedDataFor: 5,
     }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCT_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useGetProductsQuery } = productApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } =
+  productApiSlice;
