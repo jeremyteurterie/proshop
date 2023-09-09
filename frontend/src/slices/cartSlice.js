34c5026a97a1a6d2e770decfe2e84cbe3a02e46d
Jeremy <jeremy.teurterie@gmail.com>
2023-09-09 10:03:01 +0700

    :seedling: Update

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -12,15 +12,16 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = action.payload;
       const existItem = state.cartItems.find((x) => x._id === item._id);
+
       if (existItem) {
         state.cartItems = state.cartItems.map((x) =>
           x._id === existItem._id ? item : x
         );
       } else {
-        state.carteItems = [...state.cartItems, item];
+        state.cartItems = [...state.cartItems, item];
       }
 
-      return updateCart(state);
+      return updateCart(state, item);
     },
   },
 });
