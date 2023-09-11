0b628b071ea1342a74f67f0911d7fd15b1f5c0dc
Jeremy <jeremy.teurterie@gmail.com>
2023-09-11 11:08:33 +0700

    :sparkles: Feat: cart screen

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -10,6 +10,7 @@ import App from './App';
 // Screens
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const Routes = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +18,7 @@ const Routes = createBrowserRouter(
       <Route path="/" element={<App />}>
         <Route index={true} path="/" element={<HomeScreen />}></Route>
         <Route path="/product/:id" element={<ProductScreen />}></Route>
+        <Route path="/cart" element={<CartScreen />}></Route>
       </Route>
     </>
   )
