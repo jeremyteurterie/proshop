3dbe1dc1cca64abca710522aeba45ac098f05ebe
Jeremy <jeremy.teurterie@gmail.com>
2023-09-12 09:47:01 +0700

    :sparkles: Feat: login screen

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -11,6 +11,7 @@ import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const Routes = createBrowserRouter(
   createRoutesFromElements(
@@ -19,6 +20,7 @@ const Routes = createBrowserRouter(
         <Route index={true} path="/" element={<HomeScreen />}></Route>
         <Route path="/product/:id" element={<ProductScreen />}></Route>
         <Route path="/cart" element={<CartScreen />}></Route>
+        <Route path="/login" element={<LoginScreen />}></Route>
       </Route>
     </>
   )
