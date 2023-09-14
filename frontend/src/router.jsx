f09d0ebe04605eb3a27e6e1b4c43fedc0ff49488
Jeremy <jeremy.teurterie@gmail.com>
2023-09-14 09:15:29 +0700

    :sparkles: Feat: shipping screen

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -13,6 +13,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const Routes = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +24,7 @@ const Routes = createBrowserRouter(
         <Route path="/cart" element={<CartScreen />}></Route>
         <Route path="/login" element={<LoginScreen />}></Route>
         <Route path="/register" element={<RegisterScreen />}></Route>
+        <Route path="/shipping" element={<ShippingScreen />}></Route>
       </Route>
     </>
   )
